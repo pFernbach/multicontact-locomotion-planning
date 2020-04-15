c75fe7131badb2d5719897053d9f17d4ea190766
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-04-15 08:26:37 +0200

    [Config] talos_plateform: use timeopt and increase step duration

--- python/mlp/demo_configs/talos_plateformes.py
+++ python/mlp/demo_configs/talos_plateformes.py
@@ -1,4 +1,4 @@
-TIMEOPT_CONFIG_FILE = "cfg_softConstraints_talos.yaml"
+TIMEOPT_CONFIG_FILE = "cfg_softConstraints_timeopt_talos.yaml"
 from .common_talos import *
 SCRIPT_PATH = "demos"
 ENV_NAME = "multicontact/plateforme_surfaces"
@@ -7,10 +7,10 @@ kp_Eff = 500.  # proportional gain of the effectors motion task
 
 
 DURATION_INIT = 3.  # Time to init the motion
-DURATION_SS = 2.
-DURATION_DS = 0.3
+DURATION_SS = 2.5
+DURATION_DS = 0.8
 DURATION_TS = 0.4
 DURATION_CONNECT_GOAL = 0.
 
 EFF_T_PREDEF = 0.2
-p_max = 0.13
+p_max = 0.06
