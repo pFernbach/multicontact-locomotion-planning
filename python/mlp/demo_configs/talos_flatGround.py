deba76f6ba31dfb42032d43f6919e5b8e5911474
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-05-25 15:40:11 +0200

    [Config] change settings to lower com Height

--- python/mlp/demo_configs/talos_flatGround.py
+++ python/mlp/demo_configs/talos_flatGround.py
@@ -3,14 +3,15 @@ from .common_talos import *
 SCRIPT_PATH = "demos"
 ENV_NAME = "multicontact/ground"
 
-DURATION_SS = 1.2
-DURATION_DS = 0.3
+DURATION_SS = 1.6
+DURATION_DS = 0.5
 
 EFF_T_PREDEF = 0.2
 EFF_T_DELAY = 0.05
 FEET_MAX_VEL = 1.
 FEET_MAX_ANG_VEL = 1.5
 p_max = 0.07
+COM_SHIFT_Z = -0.05
 
 USE_PLANNING_ROOT_ORIENTATION = True
 GUIDE_STEP_SIZE = 0.5
