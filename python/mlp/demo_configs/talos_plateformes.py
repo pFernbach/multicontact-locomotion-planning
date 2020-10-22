23487a62b69f66f7afe051d31da9c5e116a4a46b
pFernbach <pierre.fernbach@gmail.com>
2020-10-22 20:57:37 +0200

    [Demo] update talos_platforms

--- python/mlp/demo_configs/talos_plateformes.py
+++ python/mlp/demo_configs/talos_plateformes.py
@@ -1,21 +1,27 @@
-TIMEOPT_CONFIG_FILE = "cfg_softConstraints_timeopt_talos.yaml"
+TIMEOPT_CONFIG_FILE = "cfg_softConstraints_talos.yaml"
 from .common_talos import *
 Robot.urdfSuffix += "_safeFeet"
 SCRIPT_PATH = "demos"
 ENV_NAME = "multicontact/plateforme_surfaces_noscale"
 
 kp_Eff = 2000.  # proportional gain of the effectors motion task
-DEMO_NAME = "talos_platformes_full"
+DEMO_NAME = "talos_platformes"
 
 #DURATION_INIT = 3.  # Time to init the motion
 DURATION_SS = 2.
-DURATION_DS = 0.5
+DURATION_DS = 1.5
 DURATION_TS = 0.4
 DURATION_CONNECT_GOAL = 0.
-COM_SHIFT_Z = -0.03
-COM_VIAPOINT_SHIFT_Z = -0.03
-EFF_T_PREDEF = 0.2
-p_max = 0.12
+COM_SHIFT_Z = -0.05
+COM_VIAPOINT_SHIFT_Z = -0.05
+
+EFF_T_PREDEF = 0.3
+EFF_T_PREDEF_TAKEOFF = EFF_T_PREDEF  # duration during which the motion of the end effector is forced to be orthogonal to the contact surface, at the beginning and the end of the phase
+EFF_T_PREDEF_LANDING = EFF_T_PREDEF
+p_max = 0.16
+p_max_takeoff = p_max  #setting used to compute the default height of the effector trajectory. end_effector/bezier_predef.py : computePosOffset()
+p_max_landing = p_max  #setting used to compute the default height of the effector trajectory. end_effector/bezier_predef.py : computePosOffset()
+
 """
 IK_REFERENCE_CONFIG = np.array(
 [       0.0,
@@ -60,4 +66,4 @@ IK_REFERENCE_CONFIG = np.array(
     ])
 """
 
-IK_REFERENCE_CONFIG = np.array(Robot.referenceConfig)
+IK_REFERENCE_CONFIG = np.array(Robot.referenceConfig_elbowsUp)
